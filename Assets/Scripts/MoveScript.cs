4021c5d7d9c83b74fbd747dd1d52297fa85a106d
unknown <vasved@ttu.ee>
2015-10-26 19:52:22 +0200

    initial commit

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+       
+    
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        rb.velocity = movement * speed;
+
+    }
+
+}
