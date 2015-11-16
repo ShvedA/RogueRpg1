41335be2ad3ef61e7ebd3b966b5027595277a775
vidim888 <vasved@ttu.ee>
2015-11-16 21:05:47 +0200

    Walls
    Walls ._.

---
+++ Assets/Scripts/WallPlacer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallPlacer : MonoBehaviour {
+
+    public GameObject brick;
+
+    void Start()
+    {
+        
+        for (int x = -22; x <= 22; x++)
+        {
+            for (int y = -10; y <= 10; y++)
+            {
+                if (x == -22 || x == 22 || y == -10 || y == 10)
+                {
+                    GameObject each = Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                    each.transform.parent = transform;
+                }
+                
+            }
+                
+        }
+    }
+}
