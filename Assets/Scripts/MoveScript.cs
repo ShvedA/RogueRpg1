d3e153767bb087d408ad325cbdb29e80935bfe0c
aleksandr.sved <aleksandr.sved@helmes.ee>
2015-11-23 22:29:19 +0200

    Remove fire projectiles on collision with wall

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -63,6 +63,7 @@ namespace Assets.Scripts
             var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
             position = Camera.main.ScreenToWorldPoint(position);
             var go = Instantiate(Projectile, transform.position, Quaternion.identity) as GameObject;
+            Physics2D.IgnoreCollision(go.GetComponent<Collider2D>(), GetComponent<Collider2D>());
             var projectileVector = new Vector2(position.x - transform.position.x, position.y - transform.position.y);
             if (projectileVector.x >= 0)
             {
@@ -75,6 +76,7 @@ namespace Assets.Scripts
             go.transform.parent = transform;
             //go.transform.LookAt(position);
             go.GetComponent<Rigidbody2D>().AddForce(Vector2.ClampMagnitude(projectileVector, 0.01f) * 100 * projectileSpeed);
+            
         }
 
         private double GetAngle(Vector2 vectorToCenter)
---
+++ Assets/Scripts/OnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class OnCollision : MonoBehaviour {
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.name == "Wall(Clone)"){
+                foreach (var fire in col.contacts.Where(x => x.otherCollider.gameObject.name == "Fire Projectile(Clone)"))
+                {
+                    Destroy(fire.otherCollider.gameObject);
+                }
+            }
+            
+        }
+
+    }
+}
