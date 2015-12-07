b6bd5680df3fcea0438684fa4bfd14f101934e1e
aleksandr.sved <aleksandr.sved@helmes.ee>
2015-12-08 00:07:24 +0200

    Adding Monster class and Spider class

---
+++ Assets/Scripts/Spider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Spider : Monster {
+
+        void Start()
+        {
+            transform.position = new Vector3(15f, 7f);
+        }
+
+        void Update () {
+            if (IsDead())
+            {
+                Destroy(gameObject);
+                Spider spider = new Spider();
+                Spider spider2 = new Spider();
+            }
+        }
+    }
+}
