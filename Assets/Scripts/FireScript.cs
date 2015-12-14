cd26f35e3a20d153569f61f547b0c8c59c023db9
vidim888 <vasved@ttu.ee>
2015-12-14 19:12:46 +0200

    Fire Particles

--- Assets/Scripts/FireScript.cs
+++ Assets/Scripts/FireScript.cs
@@ -19,7 +19,7 @@ namespace Assets.Scripts
         void Update () {
             if (Input.GetMouseButton(0))
             {
-                Fire();
+                Fire3();
             }
         }
 
@@ -56,6 +56,23 @@ namespace Assets.Scripts
 
             Particles.GetComponent<ParticleSystem>().SetParticles(particles, count);
         }
+
+        private void Fire3()
+        {
+            var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
+            position = Camera.main.ScreenToWorldPoint(position);
+            var projectileVector = new Vector2(position.x - transform.position.x, position.y - transform.position.y);
+
+            if (projectileVector.x >= 0)
+            {
+                Particles.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, Mathf.Atan(projectileVector.y / projectileVector.x) / Mathf.PI * 180);
+            }
+            else if (projectileVector.x < 0)
+            {
+                Particles.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, 180 + Mathf.Atan(projectileVector.y / projectileVector.x) / Mathf.PI * 180);
+            }
+        }
+
 
     }
 }
