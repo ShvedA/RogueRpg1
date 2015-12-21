5b45fdbbe57090e58ab90cdf3bf74f83e3488e4b
aleksandr.sved <aleksandr.sved@helmes.ee>
2015-12-22 00:02:22 +0200

    Refactoring shooting

--- Assets/Scripts/FireScript.cs
+++ Assets/Scripts/FireScript.cs
@@ -2,74 +2,7 @@
 
 namespace Assets.Scripts
 {
-    public class FireScript : MonoBehaviour {
+    public class FireScript : ShootingScript {
 
-        public float ProjectileSpeed;
-
-        public GameObject Projectile;
-
-        public GameObject Particles;
-
-        public float ParticleSpeed;
-
-        private float _arc;
-
-        private static readonly Vector2 ZeroVector = new Vector2(1, 0);
-
-        private void Awake()
-        {
-
-            UnityEditor.SerializedObject so = new UnityEditor.SerializedObject(Particles.GetComponent<ParticleSystem>());
-            _arc = so.FindProperty("ShapeModule.arc").floatValue;
-        }
-
-        void Update () {
-            /*if (Input.GetButton("Fire1"))
-            {
-                Fire();
-            }*/
-            
-            if (Input.GetMouseButton(0))
-            {
-                Fire3();
-            }
-            if (Input.GetButton("Fire1"))
-                Particles.GetComponent<ParticleSystem>().Play();
-            else if (Input.GetButtonUp("Fire1"))
-                Particles.GetComponent<ParticleSystem>().Stop();
-            
-        }
-
-        private float GetAngle(Vector2 vectorToCenter)
-        {
-            float angle = Vector2.Angle(vectorToCenter, ZeroVector);
-            
-            if (vectorToCenter.y < 0)
-            {
-                angle = 360 - angle;
-            }
-            return angle;
-        }
-
-        private void Fire()
-        {
-            var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
-            position = Camera.main.ScreenToWorldPoint(position);
-            var go = Instantiate(Projectile, transform.position, Quaternion.identity) as GameObject;
-            Physics2D.IgnoreCollision(go.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-            var projectileVector = new Vector2(position.x - transform.position.x, position.y - transform.position.y);
-            go.transform.Rotate(new Vector3(0, 0, GetAngle(projectileVector)));
-            go.transform.parent = transform;
-            go.GetComponent<Rigidbody2D>().AddForce(Vector2.ClampMagnitude(projectileVector, 0.001f) * 1000 * ProjectileSpeed);
-        }
-
-        private void Fire3()
-        {
-            var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
-            position = Camera.main.ScreenToWorldPoint(position);
-            var projectileVector = new Vector2(position.x - transform.position.x, position.y - transform.position.y);
-            Particles.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, GetAngle(projectileVector) - _arc / 2);
-
-        }
     }
 }
--- Assets/Scripts/MonsterMoveScript.cs
+++ Assets/Scripts/MonsterMoveScript.cs
@@ -1,30 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class MonsterMoveScript : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class MonsterMoveScript : MonoBehaviour {
 
-    public float life;
+        public float Life;
 
-    public float Speed;
+        public float Speed;
 
-    private Rigidbody2D _rb;
+        private Rigidbody2D _rb;
 
-    public GameObject character;
+        public GameObject Character;
 
-    private void Start()
-    {
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
 
-        _rb = GetComponent<Rigidbody2D>();
-
-
+        private void Update()
+        {
+            var position = Character.transform.position;
+            Vector2 movement = Vector2.ClampMagnitude(new Vector2(position.x - transform.position.x, position.y - transform.position.y), 0.001f) * 1000;
+            _rb.velocity = movement * Speed;
+        }
     }
-
-    private void Update()
-    {
-        var position = character.transform.position;
-        Vector2 movement = Vector2.ClampMagnitude(new Vector2(position.x - transform.position.x, position.y - transform.position.y), 0.001f) * 1000;
-        _rb.velocity = movement * Speed;
-
-    }
-
 }
