1bdf5027bd9723b035273096c75bbefde65f1f41
vidim888 <vasved@ttu.ee>
2016-01-12 00:18:52 +0200

    Menu

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	public void StartButton(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
