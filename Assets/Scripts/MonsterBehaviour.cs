b0af64b175e8e250e7a0f0fe5f5750ba60848207
ShvedA <aleksandr.sved@mail.ru>
2016-01-18 22:19:15 +0200

    MonsterBehaviour refactoring

--- Assets/Scripts/MonsterBehaviour.cs
+++ Assets/Scripts/MonsterBehaviour.cs
@@ -4,31 +4,14 @@ namespace Assets.Scripts
 {
     public class MonsterBehaviour : MonoBehaviour
     {
-
         public float Life;
-
         public float Speed;
-
         private Rigidbody2D _rb;
-
         public GameObject Character;
-
         private int _count = 0;
-
         private Vector2 _randomMove = new Vector2(0, 0);
-
-        public Transform sightStart,
-            sightTopEnd,
-            sightRightEnd,
-            sightDownEnd,
-            sightLeftEnd;
-
-        public bool spottedUp,
-            spottedRight,
-            spottedDown,
-            spottedLeft = false;
-
-
+        public Transform sightStart, sightTopEnd, sightRightEnd, sightDownEnd, sightLeftEnd;
+        public bool spottedUp, spottedRight, spottedDown, spottedLeft;
 
         private void Start()
         {
@@ -52,43 +35,44 @@ namespace Assets.Scripts
                     break;
             }
             _rb.velocity = _randomMove * Speed;
+            //FollowBehaviour();
+            //ChaseWhenOnSameLine();
+        }
 
-            //Follow version of a spider 
-            /*
+        private void ChaseWhenOnSameLine()
+        {
             var position = Character.transform.position;
-            Vector2 movement = Vector2.ClampMagnitude(new Vector2(position.x - transform.position.x, position.y - transform.position.y), 0.001f) * 1000;
-            _rb.velocity = movement * Speed;
-
-
-
-            //version where spider chases you if x or y coordinates were the same 
-            var position = Character.transform.position;
-            if (((int)transform.position.x).Equals((int)position.x)||((int)transform.position.y).Equals((int)position.y)||(_count<=0))
+            if (((int) transform.position.x).Equals((int) position.x) || ((int) transform.position.y).Equals((int) position.y) || (_count <= 0))
             {
                 if (_count > 0)
                 {
                     _count = -100;
                 }
-                else if ((transform.position.y > position.y)&&((int)transform.position.x).Equals((int)position.x))
+                else if ((transform.position.y > position.y) && ((int) transform.position.x).Equals((int) position.x))
                 {
-                    _randomMove = new Vector2(0,-3);
+                    _randomMove = new Vector2(0, -3);
                 }
                 else if ((transform.position.y < position.y) && ((int) transform.position.x).Equals((int) position.x))
                 {
-                    _randomMove = new Vector2(0,3);
+                    _randomMove = new Vector2(0, 3);
                 }
                 else if ((transform.position.x > position.x) && ((int) transform.position.y).Equals((int) position.y))
                 {
-                    _randomMove = new Vector2(-3,0);
+                    _randomMove = new Vector2(-3, 0);
                 }
                 else if ((transform.position.x < position.x) && ((int) transform.position.y).Equals((int) position.y))
                 {
-                    _randomMove = new Vector2(3,0);
+                    _randomMove = new Vector2(3, 0);
                 }
             }
-            */
         }
 
+        private void FollowBehaviour()
+        {
+            var position = Character.transform.position;
+            Vector2 movement = Vector2.ClampMagnitude(new Vector2(position.x - transform.position.x, position.y - transform.position.y), 0.001f) * 1000;
+            _rb.velocity = movement * Speed;
+        }
 
         private void RayCasting()
         {
@@ -96,18 +80,10 @@ namespace Assets.Scripts
             Debug.DrawLine(sightStart.position, sightRightEnd.position, Color.green);
             Debug.DrawLine(sightStart.position, sightDownEnd.position, Color.green);
             Debug.DrawLine(sightStart.position, sightLeftEnd.position, Color.green);
-            spottedUp = Physics2D.Linecast(
-                sightStart.position, sightTopEnd.position,
-                1 << LayerMask.NameToLayer("Character"));
-            spottedRight = Physics2D.Linecast(
-                sightStart.position, sightRightEnd.position,
-                1 << LayerMask.NameToLayer("Character"));
-            spottedDown = Physics2D.Linecast(
-                sightStart.position, sightDownEnd.position,
-                1 << LayerMask.NameToLayer("Character"));
-            spottedLeft = Physics2D.Linecast(
-                sightStart.position, sightLeftEnd.position,
-                1 << LayerMask.NameToLayer("Character"));
+            spottedUp = Physics2D.Linecast(sightStart.position, sightTopEnd.position, 1 << LayerMask.NameToLayer("Character"));
+            spottedRight = Physics2D.Linecast(sightStart.position, sightRightEnd.position, 1 << LayerMask.NameToLayer("Character"));
+            spottedDown = Physics2D.Linecast(sightStart.position, sightDownEnd.position, 1 << LayerMask.NameToLayer("Character"));
+            spottedLeft = Physics2D.Linecast(sightStart.position, sightLeftEnd.position, 1 << LayerMask.NameToLayer("Character"));
 
         }
 
