efa8d6d385b39b00bb977850fa89f621ba1939d3
vidim888 <vasved@ttu.ee>
2016-01-26 00:39:11 +0200

    Обои and minor things

---
+++ Assets/Scripts/FloorScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class FloorScript : MonoBehaviour
+    {
+
+        [SerializeField] private GameObject _floorTexture;
+        private const int Width = 10;
+        private const int Height = 10;
+
+        void Start ()
+        {
+            for (int x = -Width; x <= Width; x++)
+            {
+                for (int y = -Height; y <= Height; y++)
+                {
+                        GameObject each = Instantiate(_floorTexture, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                        each.transform.parent = transform;
+                }
+            }
+        }
+    }
+}
