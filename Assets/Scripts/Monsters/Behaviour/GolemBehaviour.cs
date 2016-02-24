17083a28d6e0b6709c8c6c913da4f561e7547c4d
ShvedA <alex.shved@mail.ru>
2016-02-25 01:46:31 +0200

    fixed spider animation at moving and added same movement for golem

--- Assets/Scripts/Monsters/Behaviour/GolemBehaviour.cs
+++ Assets/Scripts/Monsters/Behaviour/GolemBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Scripts.Helper;
 using UnityEngine;
 
 namespace Assets.Scripts.Monsters.Behaviour {
@@ -14,7 +15,15 @@ namespace Assets.Scripts.Monsters.Behaviour {
 
         private void Movement()
         {
-            throw new NotImplementedException();
+            var position = Character.transform.position;
+            var vectorFromCenter = new Vector2(position.x - transform.position.x, position.y - transform.position.y);
+            double angle = AngleHelper.GetAngleForTurningAround(vectorFromCenter);
+            var angleFromSectorBeginning = angle + (Constants.Round / NumOfSpriteTurns) / 2;
+
+            int spriteNr = (int)(angleFromSectorBeginning / (Constants.Round / NumOfSpriteTurns));
+            if (spriteNr < 0 || spriteNr == NumOfSpriteTurns) {
+                spriteNr = 0;
+            }
         }
     }
 }
